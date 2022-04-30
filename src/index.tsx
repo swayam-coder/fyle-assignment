5cd720bad53d4cccb044be9bc4473b62998c005e
swayam-coder <swayam14feb@gmail.com>
2022-04-30 12:15:57 +0530

    shift from react18 to react17 due to lack of types

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Toaster } from "react-hot-toast";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+<>
+   <App /> 
+   <Toaster />
+</>,        
+document.getElementById("root"));
