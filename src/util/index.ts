bdaa1cd82acb64d84677b50c25bfa18bf09aadb8
swayam-coder <swayam14feb@gmail.com>
2022-04-30 12:25:37 +0530

    added toasts for failed requests/ bad response

---
+++ src/util/index.ts
@@ -0,0 +1,30 @@
+import toast from "react-hot-toast";
+import { REQUESTS } from "src/types";
+
+export async function getInfo(url: string, type: string) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Response Not OK");
+    }
+
+    return await response.json()
+  } catch (error) {
+    if(type === REQUESTS.GET_USER_INFO && error instanceof Error && error.message.localeCompare("Not Found")) {
+      toast.error("Not Found", {
+        style: {
+          background: "red",
+          color: "white"
+        },
+        iconTheme: {
+          secondary: "red",
+          primary: "white"
+        }
+      })
+    } else {
+      toast.error("Something wrong happened");
+    }
+    console.log(error);
+  }
+}
