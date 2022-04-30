b0a335d6e008fe82a61b51fe7f9596a5d84376ed
swayam-coder <swayam14feb@gmail.com>
2022-04-30 12:16:38 +0530

    added enum for request types

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface IUserContext {
+  userInfo: any;
+  setuserInfo: React.Dispatch<React.SetStateAction<undefined>>;
+  repoInfo: any;
+  setrepoInfo: React.Dispatch<React.SetStateAction<undefined>>;
+  currPage: any;
+  setCurrPage: React.Dispatch<React.SetStateAction<number>>;
+  pages: any;
+  setPages: React.Dispatch<React.SetStateAction<number>>;
+  repourl: any;
+  setRepourl: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export enum REQUESTS {
+  GET_USER_INFO = "GET_USER_INFO",
+  GET_REPO_INFO = "GET_REPO_INFO",
+}
