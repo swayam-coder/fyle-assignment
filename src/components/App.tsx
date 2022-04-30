b6dd290f2533fd53c488133eefa085b6f71273b4
swayam-coder <swayam14feb@gmail.com>
2022-04-30 12:17:35 +0530

    setup wouter and code splitting using React.lazy

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Route } from "wouter";
+import ContextProvider from "../contexts/user";
+
+const Input = React.lazy(() => import("./Input")) 
+const Profile = React.lazy(() => import("./Profile"))
+
+export default function App() {
+  return (
+    <>
+      <ContextProvider>
+        <React.Suspense fallback>
+          <Route path="/" component={Input} />
+          <Route path="/userinfo/:name" component={Profile} />
+        </React.Suspense>
+      </ContextProvider>
+    </>
+  );
+}
